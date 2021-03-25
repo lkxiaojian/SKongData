0c808ae25c6e8de042b949d4cd2ea24b5d9a01a2
lk <18513075404@163.com>
2021-03-25 13:42:27 +0800

    mine

--- basics_main/src/main/java/com/zky/basics/main/mvvm/viewmodel/MainViewModel.kt
+++ basics_main/src/main/java/com/zky/basics/main/mvvm/viewmodel/MainViewModel.kt
@@ -6,9 +6,11 @@ import android.view.View
 import androidx.databinding.ObservableField
 import com.alibaba.android.arouter.launcher.ARouter
 import com.zky.basics.api.common.entity.task.TaskBean
+import com.zky.basics.api.config.API
 import com.zky.basics.common.constant.Constants.appCode
 import com.zky.basics.common.event.SingleLiveEvent
 import com.zky.basics.common.mvvm.viewmodel.BaseRefreshViewModel
+import com.zky.basics.common.util.spread.decode
 import com.zky.basics.main.R
 import com.zky.basics.main.mvvm.model.MainModel
 
@@ -18,10 +20,13 @@ class MainViewModel(application: Application, model: MainModel) :
     private var mVoidSingleLiveEvent: SingleLiveEvent<String>? = null
     var nameTop = ObservableField<String>()
     var nameBom = ObservableField<String>()
-
+    var errorHeard=ObservableField<Int>()
+    var imageHeard=ObservableField<String>()
     init {
         nameTop.set("某某县时空大数据采集")
         nameBom.set("与查询系统")
+        errorHeard.set(R.drawable.mine_tmp)
+        imageHeard.set(API.ImageFolderPath + "".decode("headImgPath"))
     }
 
     override fun refreshData() {
